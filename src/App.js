8778c2a3eec98bb10aa60909766fbdc2e365fd58
Zach45 <Zach45@users.noreply.github.com>
2020-08-13 11:35:11 -0700

    I now can switch pages hahahah

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 class App extends Component {
 
   render(){
     return(    
-        <div>
-          <Homepage />
-        </div>  
+      <div>
+        <Switch>
+          <Route exact path='/' component={Homepage} /> 
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
+      </div>  
     );
   }
 }
