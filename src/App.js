eefceed9d85488480e26d22d92a1c76e1a8cde3f
Zach45 <Zach45@users.noreply.github.com>
2020-08-13 16:53:28 -0700

    my collection-item and collection-preview components are now done along with their styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
 
 class App extends Component {
+
 
   render(){
     return(    
       <div>
         <Switch>
           <Route exact path='/' component={Homepage} /> 
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>  
     );
